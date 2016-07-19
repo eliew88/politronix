9e2c785c2318033421ccd32223f94ce21dd871ad
George Horrell <ghorrell@stanford.edu>
2016-07-18 18:45:52 -0700

    Added a MySQL setup service.

--- other/dbinit_script.sh
+++ other/dbinit_script.sh
@@ -6,6 +6,7 @@
 EXPECTED_ARGS=2
 E_BADARGS=65
 MYSQL=`which mysql`
+MYSQL_SERVICE="mysql"
 
 Q1="CREATE DATABASE IF NOT EXISTS POLITRONIX;"
 Q2="GRANT USAGE ON POLITRONIX.* TO $1@localhost IDENTIFIED BY '$2';"
@@ -17,12 +18,26 @@ TABLE_CREATE="CREATE TABLE data (id INT UNSIGNED AUTO_INCREMENT, topic VARCHAR(2
 
 ALL="${DB_CREATE}${SELECT_DB}${TABLE_CREATE}"
 
+
+
+
 if [ $# -ne $EXPECTED_ARGS ]
 then
     echo "Usage: $0 dbuser dbpass"
     exit $E_BADARGS
 fi
 
+if (( $(ps -ef | grep -v grep | grep $MYSQL_SERVICE | wc -l ) > 0 )) 
+then 
+    echo "MySQL server is running!"
+else
+    echo "Starting MySQL server."
+    /etc/init.d/$MYSQL_SERVICE start
+    echo "MySQL server started successfully."
+fi
+
 set -e
+echo "Attempting to add new user, database and data table to your MySQL server."
+echo "Enter your MySQL ROOT password in the ensuing prompt."
 $MYSQL -uroot -p -e "$ALL"
-echo "POLITRONIX db and data table successfully created"
+echo "POLITRONIX db and table successfully created. Thanks for flying with GWPH airways."
