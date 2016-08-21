220475ddb0e0a971c81ea4b644b0fc5317dc5004
Zachary Bordofsky <ZEB@Zacharys-MBP.local>
2016-08-21 16:26:33 -0700

    writes sentiment to database

--- STREAM/src/tweetProcess.h
+++ STREAM/src/tweetProcess.h
@@ -1,5 +1,6 @@
 #include <string> 
 #include <vector> 
+#include <map> 
 
 
 
@@ -9,9 +10,15 @@ class TweetProcess {
 		TweetProcess();
 		void writeToBuffer(std::string input);
 		void processTweet(); 
+		double score_tweet(std::string tweet, std::map<std::string, double>& word_scores);
+		std::map<std::string, double> create_map();
+		std::string trim_word(std::string untrimmed);
+		void writeToDatabase(std::string tweet, std::string time, double score); 
+		std::string get_current_time(); 
 
 	private: 
 		char m_buffer[1000000]; 
 		std::vector<std::string> m_tweets; 
 		int m_buffPlace; 
+		std::map<std::string, double> m_sentiWordScores; 
 }; 
