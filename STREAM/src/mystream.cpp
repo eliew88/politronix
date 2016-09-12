c6afac08ae67fdf3bafeae9a51ebb78384829340
EC2 Default User <ec2-user@ip-172-31-7-140.us-west-2.compute.internal>
2016-09-12 22:56:59 +0000

    Error handling updated

--- STREAM/src/mystream.cpp
+++ STREAM/src/mystream.cpp
@@ -21,6 +21,8 @@ const char *access_token_secret = "EPyeeT8wPaFJWQpUOPB7unOVN8UKDIPCvnHogzp9GtADm
 bool local;
 int num_disconnects;
 int num_tweets;
+//number of unsuccessful attempts to connect to twitter before getting data
+int num_attempts;
 
 /* Function: handle_disconnect
  * Usage: handle_disconnect(curl_disconnect_error_code, n_disconnects)
@@ -43,43 +45,36 @@ void sleepmil(int n_millisecs) {
 	usleep(n_millisecs * 1000);
 }
 
-bool handle_disconnect(int error_code, int n_disconnects) {
-	/*
-	if(error_code == 500) {
-		int sleep_time = 250 * (n_disconnects + 1);
+bool handle_disconnect(int error_code, int n) {
+	printf("SLEEPING FOR HTTP CODE: %d \n", error_code);
+	if (error_code == 500) {
+		int sleep_time = 250 * (n + 1);
 		sleepmil(min(sleep_time, 16*1000));
-		return true;
-	} else if(error_code > 500) {
-		int sleep_time = (5*1000) * pow(2, n_disconnects);
+	} else if (error_code > 500) {
+		int sleep_time = (5*1000) * pow(2, n);
 		sleepmil(min(sleep_time, 5*60*1000));
-		return true;
-	} else if(error_code >= 420 && error_code <= 429) {
-		int sleep_time = (1*1000) * pow(2, n_disconnects);
+	} else if (error_code >= 420 && error_code <= 499) {
+		int sleep_time = (1*1000) * pow(2, n);
 		sleepmil(sleep_time);
-		return true;
-	} else if (error_code == 18) {
-		sleepmil(30000);
-		return true;
 	} else {
 		return false;
 	}
-	*/
 
-	sleepmil(60000);
 	return true;
 }
 
 //called everytime new input is recieved, calls member function to parse input into tweets
 size_t tweet_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+	num_attempts = 0;
 	size_t total = size * nmemb; 
 	string str = string(ptr, total); 
 	TweetProcess *tweets = reinterpret_cast<TweetProcess*>(userdata); 
-	
+
 	num_tweets += tweets->writeToBuffer(str, local); 
-        if (num_tweets % STATS_UPDATE_INTERVAL == 0) { //print STATS update periodically
-                printf("STATS UPDATE\nNumber of tweets processed: %i\n", num_tweets);
-                printf("Number of disconnects: %i\n", num_disconnects);
-        }
+	if (num_tweets % STATS_UPDATE_INTERVAL == 0) { //print STATS update periodically
+		printf("STATS UPDATE\nNumber of tweets processed: %i\n", num_tweets);
+		printf("Number of disconnects: %i\n", num_disconnects);
+	}
 
 	return total; 
 }
@@ -109,42 +104,36 @@ int main(int argc, char *argv[]) {
 	// libcurl will now fail on an HTTP error (>=400)
 	curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
 
+	// prepare for errors
+	char error_buffer[1000];
+	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, error_buffer);
+
 	// In this case, we're not specifying a callback function for
 	// handling received data, so libcURL will use the default, which
 	// is to write to the file specified in WRITEDATA
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, tweets);
 
 	//when data comes it, this calls our write_callback function 
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, tweet_callback); 
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, tweet_callback);	
 
-	// Execute the request!
-	num_disconnects = 0;
-	int curlstatus = curl_easy_perform(curl);
-	printf("curl_easy_perform terminated with status code %d\n", curlstatus);
-
-	while (curlstatus != 0) {
-		//Disconnected from streaming: handle disconnect and exit if needed, otherwise continue streaming
+	while (true) {
+		// Execute the request!
+		int curlstatus = curl_easy_perform(curl);
+		printf("curl_easy_perform terminated with status code %d\n", curlstatus);
 		num_disconnects++;
-		printf("Disconnect #%i... \n", num_disconnects);
-		if (!handle_disconnect(curlstatus, num_disconnects)) {
+		printf("Disconnect #%i: %s \n", num_disconnects, error_buffer);
+
+		//get the error code
+		long http_code = 0;
+		curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+		//Disconnected from streaming: handle disconnect and exit if needed, otherwise continue streaming
+		if (!handle_disconnect(http_code, num_attempts)) {
 			// Disconnect error code could not be handled: terminate program
-			printf("curl error could not be handled: Error Code %i:Streaming terminating...\n ", curlstatus);
+			printf("curl error could not be handled: Error Code: %ld, Curl Status: %i \n ", http_code, curlstatus);
 			break;
 		}
-		curl = curl_easy_init();
-		// URL we're connecting to
-		curl_easy_setopt(curl, CURLOPT_URL, signedurl);
-		// User agent we're going to use, fill this in appropriately
-		curl_easy_setopt(curl, CURLOPT_USERAGENT, "Politronix/0.1");
-		// libcurl will now fail on an HTTP error (>=400)
-		curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
-		// In this case, we're not specifying a callback function for
-		// handling received data, so libcURL will use the default, wh
-		// is to write to the file specified in WRITEDATA
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, tweets);
-		// when data comes it, this calls our write_callback function 
-		// curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, tweet_callback);
-		curlstatus = curl_easy_perform(curl);
+		num_attempts++;
 	}
 
 	curl_easy_cleanup(curl);
